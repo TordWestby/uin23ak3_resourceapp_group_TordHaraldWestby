9e72bae519565186f9dae88c985f142a77a1000a
Tord Westby <102856887+TordWestby@users.noreply.github.com>
2024-03-19 17:08:04 +0100

    oppsett
    Satt opp react og sass til prosjektet..

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './css/main.scss';
+
+function App() {
+  return (
+      <div className='main'>
+        <div className='content'>
+          <h1>Hello World</h1>
+        </div>
+      </div>
+  );
+}
+
+export default App;
